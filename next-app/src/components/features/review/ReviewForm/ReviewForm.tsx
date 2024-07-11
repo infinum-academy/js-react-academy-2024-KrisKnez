7e079de91627e1031286056fc9e5f5f43b91dca4
KrisKnez <kristijankej@gmail.com>
2024-07-11 15:32:34 +0200

    refactor: form from controlled to uncontrolled using onSubmit

--- next-app/src/components/features/review/ReviewForm/ReviewForm.tsx
+++ next-app/src/components/features/review/ReviewForm/ReviewForm.tsx
@@ -9,77 +9,49 @@ interface ReviewFormProps {
 }
 
 const ReviewForm: React.FC<ReviewFormProps> = ({ addShowReview }) => {
-  const [comment, setComment] = useState("");
   const [rating, setRating] = useState(5);
 
-  const [commentIsInvalid, setCommentIsInvalid] = useState(false);
-
-  const isValidComment = (comment: string): boolean => {
-    if (!comment) return false;
-
-    return true;
-  };
-  const isValidRating = (rating: number): boolean => {
-    if (!(rating >= 1 && rating <= 5)) return false;
-
-    return true;
-  };
-
-  // Validate form, set invalid states and return true if invalid
-  const validateForm = (): boolean => {
-    let formIsInvalid = false;
-    if (!isValidComment(comment)) {
-      setCommentIsInvalid(true);
-      formIsInvalid = true;
-    }
-    if (!isValidRating(rating)) {
-      formIsInvalid = true;
-    }
-
-    return formIsInvalid;
-  };
-
-  const resetForm = () => {
-    setComment("");
-    setRating(5);
-    setCommentIsInvalid(false);
-  };
-
   return (
-    <Stack alignItems="flex-start">
-      <Heading as="h2" size="md">Reviews</Heading>
+    <Stack
+      as="form"
+      alignItems="flex-start"
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        const form = event.target as HTMLFormElement;
+
+        // Get form values
+        const formData = new FormData(form);
+        const comment = formData.get("comment") as string;
+
+        addShowReview(comment, rating);
+
+        // Reset form
+        setRating(5);
+        form.reset();
+      }}
+    >
+      <Heading as="h2" size="md">
+        Reviews
+      </Heading>
       <Textarea
+        name="comment"
+        required
         placeholder="Add review"
         maxH={200}
         bg="white"
         color="black"
-        value={comment}
-        onChange={(e) => {
-          const newComment = e.target.value;
-          setComment(newComment);
-          setCommentIsInvalid(!isValidComment(newComment));
-        }}
-        isInvalid={commentIsInvalid}
       />
       <Box p={2}>
         <RatingInput
           label={`${rating} / 5`}
+          value={rating}
           onChange={(rating) => {
             setRating(rating);
           }}
-          value={rating}
         />
       </Box>
-      <Button
-        colorScheme="gray"
-        fontSize="sm"
-        onClick={() => {
-          if (validateForm()) return;
-
-          addShowReview(comment, rating);
-          resetForm();
-        }}
-      >
+      <Button type="submit" colorScheme="gray" fontSize="sm">
         Post
       </Button>
     </Stack>
