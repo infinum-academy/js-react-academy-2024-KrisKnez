07bce50f3b651c77775d49f5d6d6df38f503a39a
KrisKnez <kristijankej@gmail.com>
2024-07-17 05:20:48 +0200

    refactor: move delete logic outside of ReviewItem component

--- next-app/src/components/features/review/ReviewList/ReviewList.tsx
+++ next-app/src/components/features/review/ReviewList/ReviewList.tsx
@@ -2,16 +2,26 @@ import { IReview } from "@/typings/review";
 import { Stack } from "@chakra-ui/react";
 import React from "react";
 import ReviewItem from "../ReviewItem/ReviewItem";
+import {
+  deleteReview,
+  useReviewDispatch,
+} from "@/contexts/review/ReviewContext";
 
 interface IReviewListProps {
   reviews: IReview[];
 }
 
 const ReviewList = ({ reviews }: IReviewListProps) => {
+  const reviewDispatch = useReviewDispatch();
+
   return (
     <Stack spacing={2}>
       {reviews.map((review, index) => (
-        <ReviewItem key={index} review={review} />
+        <ReviewItem
+          key={index}
+          review={review}
+          onDelete={(review) => reviewDispatch(deleteReview(review))}
+        />
       ))}
     </Stack>
   );
